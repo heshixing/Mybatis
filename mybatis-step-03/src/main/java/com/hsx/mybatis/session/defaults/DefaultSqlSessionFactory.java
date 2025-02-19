9a2ed89ff2c33ad5e9321cdd4ec3ab5c3d46b799
“hsx” <2372781727@qq.com>
2025-02-19 15:11:14 +0800

    “Mapper XML的解析和注册使用”

---
+++ mybatis-step-03/src/main/java/com/hsx/mybatis/session/defaults/DefaultSqlSessionFactory.java
@@ -0,0 +1,27 @@
+package com.hsx.mybatis.session.defaults;
+
+import com.hsx.mybatis.binding.MapperRegistry;
+import com.hsx.mybatis.session.SqlSession;
+import com.hsx.mybatis.session.SqlSessionFactory;
+
+/**
+ * @description:
+ * @author：何世兴，微信：MrHe1006
+ * @date: 2025-02-19
+ * @author:2372781727@qq.com
+ */
+
+public class DefaultSqlSessionFactory implements SqlSessionFactory {
+
+    private final MapperRegistry mapperRegistry;
+
+    public DefaultSqlSessionFactory(MapperRegistry mapperRegistry) {
+        this.mapperRegistry = mapperRegistry;
+    }
+
+    @Override
+    public SqlSession openSession() {
+        return new DefaultSqlSession(mapperRegistry);
+    }
+
+}
